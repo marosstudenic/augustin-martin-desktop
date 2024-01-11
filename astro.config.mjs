c53943408b689ecf46632a1ef15e0bb8e504e159
Maros Studenic <studenic.maros@gmail.com>
2024-01-11 14:32:52 +0000

    fix tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
 import preact from '@astrojs/preact';
-
 import tailwind from "@astrojs/tailwind";
+
 
 // https://astro.build/config
 export default defineConfig({
@@ -12,6 +12,5 @@ export default defineConfig({
     routing: {
       prefixDefaultLocale: false
     }
-  },
-
-})
+  }
+});
